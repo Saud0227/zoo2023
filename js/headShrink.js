7235ed06917716a993dbf68d3c3959933217bdba
Saud0227 <saud02tech@gmail.com>
2022-12-08 11:29:41 +0100

    did js

---
+++ js/headShrink.js
@@ -0,0 +1,31 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("header").style.fontSize = "0.5em";
+
+    document.getElementById("header").style.height = "5vh";
+    document.getElementById("header").style.borderRadius = "10px";
+    document.getElementById("header").style.width = "90%";
+    document.getElementById("header").style.left = "5%";
+    document.getElementById("header").style.top = "3%";
+
+
+
+  } else {
+    document.getElementById("header").style.fontSize = "1em";
+
+    document.getElementById("header").style.height = "10vh";
+    document.getElementById("header").style.borderRadius = "0";
+    document.getElementById("header").style.width = "100%";
+    document.getElementById("header").style.left = "0";
+    document.getElementById("header").style.top = "0";
+
+
+    // top: 3%;
+    // left: 5%;
+    // width: 90%;
+
+  }
+}
+
