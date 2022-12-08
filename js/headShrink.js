bcd9c76b553e7eee5af9799a088da8dd1069f76e
Saud0227 <saud02tech@gmail.com>
2022-12-08 22:23:01 +0100

    redid layout main page

--- js/headShrink.js
+++ js/headShrink.js
@@ -1,8 +1,10 @@
+const scrollTrigger = 100;
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    document.getElementById("header").style.fontSize = "0.5em";
+  if (document.body.scrollTop > scrollTrigger || document.documentElement.scrollTop > scrollTrigger) {
+    document.getElementById("header").style.fontSize = "1em";
 
     document.getElementById("header").style.height = "5vh";
     document.getElementById("header").style.borderRadius = "10px";
@@ -10,16 +12,20 @@ function scrollFunction() {
     document.getElementById("header").style.left = "2.5%";
     document.getElementById("header").style.top = "3%";
 
+    // document.getElementById("nav").style.width = "225px";
+
 
 
   } else {
-    document.getElementById("header").style.fontSize = "1em";
+    document.getElementById("header").style.fontSize = "1.5em";
 
     document.getElementById("header").style.height = "10vh";
     document.getElementById("header").style.borderRadius = "0";
     document.getElementById("header").style.width = "100%";
     document.getElementById("header").style.left = "0";
     document.getElementById("header").style.top = "0";
+
+    // document.getElementById("nav").style.width = "450px";
 
 
     // top: 3%;
