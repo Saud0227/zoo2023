694f816fc102a36a6de59afc85667cb1a0c7db62
Saud0227 <saud02tech@gmail.com>
2023-01-06 16:09:38 +0100

    did color and style correcting

--- js/headShrink.js
+++ js/headShrink.js
@@ -4,9 +4,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > scrollTrigger || document.documentElement.scrollTop > scrollTrigger) {
-    document.getElementById("header").style.fontSize = "1em";
+    document.getElementById("header").style.fontSize = "1.5em";
 
-    document.getElementById("header").style.height = "5vh";
+    document.getElementById("header").style.height = "6vh";
     document.getElementById("header").style.borderRadius = "0 0 10px 10px";
     document.getElementById("header").style.width = "95%";
     document.getElementById("header").style.left = "2.5%";
@@ -17,7 +17,7 @@ function scrollFunction() {
 
 
   } else {
-    document.getElementById("header").style.fontSize = "1.5em";
+    document.getElementById("header").style.fontSize = "2em";
 
     document.getElementById("header").style.height = "10vh";
     document.getElementById("header").style.borderRadius = "0";
