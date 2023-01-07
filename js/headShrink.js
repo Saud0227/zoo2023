071c919d14f5073b73c43aba0e1b28f1dbe76fd9
Saud0227 <saud02tech@gmail.com>
2023-01-07 01:24:58 +0100

    reskin in progress

--- js/headShrink.js
+++ js/headShrink.js
@@ -7,7 +7,7 @@ function scrollFunction() {
     document.getElementById("header").style.fontSize = "1.5em";
 
     document.getElementById("header").style.height = "6vh";
-    document.getElementById("header").style.borderRadius = "0 0 10px 10px";
+    document.getElementById("header").style.borderRadius = "0 0 15px 15px";
     document.getElementById("header").style.width = "95%";
     document.getElementById("header").style.left = "2.5%";
     // document.getElementById("header").style.top = "3%";
